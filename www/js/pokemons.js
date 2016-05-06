e16c170ace6fd275ccca4df6695fb7ab1ba4a8bd
tjvanos <tjos@student.avans.nl>
2016-05-06 14:59:18 +0200

    endless scroll

--- www/js/pokemons.js
+++ www/js/pokemons.js
@@ -1,31 +1,58 @@
 document.addEventListener("deviceready", setup, false);
 
-function setup(){
+function setup() {
     loadCompendium();
 }
 
-$(document).on('click', '#open_menu', function(){   
-    $.mobile.activePage.find('#menu').panel("open");       
+$(document).on('click', '#open_menu', function () {
+    $.mobile.activePage.find('#menu').panel("open");
 });
 
-$(document).on("swiperight", function() {
-    $.mobile.activePage.find('#menu').panel("open");    
+$(document).on("swiperight", function () {
+    $.mobile.activePage.find('#menu').panel("open");
 });
 
 var total_pokemons = 721;
 var poke_id = 0;
+var i = 0;
+var pokelist = [];
 
 function loadCompendium() {
 	var listContent = '';
 
-	$.getJSON('http://pokeapi.co/api/v2/pokemon/?limit=' + total_pokemons, function(data) {
 
-		$.each(data.results, function() {
-			poke_id++;
-			listContent += '<li><a href="#" class="pokemonListItem" rel="' + this.url + '">#' + poke_id + ' ' + this.name + '</a></li>';
+
+	$.getJSON('http://pokeapi.co/api/v2/pokemon/?limit=' + total_pokemons, function (data) {
+
+		$.each(data.results, function () {
+			pokelist.push(this);
 		});
+		console.log(pokelist);
+		for (i = 0; i < 50; i++) {
+			listContent += '<li><a href="#" class="pokemonListItem" rel="' + pokelist[i].url + '">#' + i + ' ' + pokelist[i].name + '</a></li>';
+		}
+
 
 		$('#compendiumListView').html(listContent);
 		$('#compendiumListView').listview("refresh");
 	});
 };
+
+$(document).ready(function () {
+	var win = $(window);
+
+	// Each time the user scrolls
+	win.scroll(function () {
+		// End of the document reached?
+		if ($(document).height() - win.height() == win.scrollTop()) {
+			var addNumbers=i+50;
+			for (i; i < addNumbers; i++) {
+				if(i==total_pokemons){
+					break;
+				}
+				$('#compendiumListView').append('<li><a href="#" class="pokemonListItem" rel="' + pokelist[i].url + '">#' + i + ' ' + pokelist[i].name + '</a></li>');
+
+			}
+		}
+	});
+});
