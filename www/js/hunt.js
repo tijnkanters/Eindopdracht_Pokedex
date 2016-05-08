a8de4ceda312d8a0a60d994db8dd9c3692c7cf23
Tijn Kanters <trkanter@avans.nl>
2016-05-08 23:16:58 +0200

    removed debugger

--- www/js/hunt.js
+++ www/js/hunt.js
@@ -50,7 +50,6 @@ function hunt() {
     var found = false;
     //    { xt: 51.693486, yt: 5.296459, xb: 51.693116, yb: 5.297233 },//Grass Company
     for (var i = 0; i < cords.length; i++) {
-        debugger;
         if (myPlace.x <= cords[i].xt && myPlace.x >= cords[i].xb && myPlace.y <= cords[i].yt && myPlace.y >= cords[i].yb) {
             //correct cordinates catch pokemon
 
